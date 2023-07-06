9fca15e7a434b0452b3fa79c2148b9fac8c8f3a2
nikolas.lucansky <nikolas.lucansky@globallogic.com>
2023-07-06 16:26:56 +0200

    Adding InjectSecret.py

--- KeyVault/InjectSecret.py
+++ KeyVault/InjectSecret.py
@@ -1,1 +1,34 @@
+import base64
+from azure.identity import AzureCliCredential
+from azure.keyvault.secrets import SecretClient
+from azure.core.exceptions import AzureError
 
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("--vault_url", dest="vault_url", help="")
+parser.add_option("--secret_name", dest="secret_name", help="")
+parser.add_option("--path_to_file", dest="path_to_file", help="")
+(options, args) = parser.parse_args()
+
+# Read file contents and encode in Base64
+try:
+    with open(options.path_to_file, 'rb') as file:
+        file_content = file.read()
+        base64_content = base64.b64encode(file_content).decode('utf-8')
+except FileNotFoundError:
+    print(f'File "{options.path_to_file}" not found.')
+except Exception as ex:
+    print('An error occurred while reading the file:')
+    print(ex)
+
+credential = AzureCliCredential()
+secret_client = SecretClient(vault_url=options.vault_url, credential=credential)
+
+if __name__ == '__main__':
+    try:
+        secret = secret_client.set_secret(options.secret_name, base64_content)
+        print(f'{secret.properties.version} | {options.secret_name} [✓]')
+    except AzureError as ex:
+        print('An error occurred while uploading the secret to Azure Key Vault:')
+        print(ex)
