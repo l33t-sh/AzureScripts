9c3d51d669d647102cb55d9cae1baed24057b193
NikolasLucansky <51444462+nlucansk@users.noreply.github.com>
2023-07-07 08:59:15 +0200

    Adding config file logic

--- KeyVault/InjectSecret.py
+++ KeyVault/InjectSecret.py
@@ -1,33 +1,37 @@
 import base64
+import yaml
 from azure.identity import AzureCliCredential
 from azure.keyvault.secrets import SecretClient
 from azure.core.exceptions import AzureError
-
 from optparse import OptionParser
+
+
 
 parser = OptionParser()
 parser.add_option("--vault_url", dest="vault_url", help="Please specify the Azure KeyVault endpoint")
 parser.add_option("--secret_name", dest="secret_name", help="Please specify the secret name")
-parser.add_option("--path_to_file", dest="path_to_file", help="Please specify a path to the file that needs to be injected as secret")
+parser.add_option("--path_to_file", dest="path_to_file", help="Please specify a path to the file.yml that needs to be injected as secret")
 (options, args) = parser.parse_args()
 
 try:
     with open(options.path_to_file, 'rb') as file:
-        file_content = file.read()
-        base64_content = base64.b64encode(file_content).decode('utf-8')
+        local_config = yaml.safe_load(file)
 except FileNotFoundError:
     print(f'File "{options.path_to_file}" not found.')
+except yaml.YAMLError as exc:
+            print(exc)
 except Exception as ex:
-    print('An error occurred while reading the file:')
+    print('An error occurred while reading the file.yml:')
     print(ex)
 
 credential = AzureCliCredential()
 secret_client = SecretClient(vault_url=options.vault_url, credential=credential)
 
 if __name__ == '__main__':
-    try:
-        secret = secret_client.set_secret(options.secret_name, base64_content)
-        print(f'{secret.properties.version} | {options.secret_name} [✓]')
-    except AzureError as ex:
-        print('An error occurred while uploading the secret to Azure Key Vault:')
-        print(ex)
+    for secret in local_config['secrets']:
+        try:
+            enc_secret = secret_client.set_secret(secret['name'], base64.b64encode(secret['value'].encode()).decode())
+            print(f"{enc_secret.properties.version} | {secret['name']} [✓]")
+        except AzureError as ex:
+            print('An error occurred while uploading the secret to Azure Key Vault:')
+            print(ex)
